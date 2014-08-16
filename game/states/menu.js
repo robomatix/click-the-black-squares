b956ed24a788c46c5e5d9506e7401a36a613940f
robomatix <robomatix@robomatix.net>
2014-08-16 09:34:52 +0200

    Menu : Explanation in pitch + duration

--- game/states/menu.js
+++ game/states/menu.js
@@ -18,13 +18,17 @@ Menu.prototype = {
         this.game.add.tween(this.titleText).to({y: 10}, 1000).easing(Phaser.Easing.Bounce.Out).start();
 
         // Pitch
-        this.pitchText = this.game.add.bitmapText(120, 500, 'fontSquareBB', 'THIRTY THREE SECONDS !!!', 22);
-        this.game.add.tween(this.pitchText).to({y: 200}, 1000).easing(Phaser.Easing.Bounce.Out).delay(1250).start();
+        this.pitchText = this.game.add.bitmapText(69, 500, 'fontSquareBB', 'to win a random number of points !!!', 22);
+        this.game.add.tween(this.pitchText).to({y: 60}, 1000).easing(Phaser.Easing.Bounce.Out).delay(1250).start();
+
+        // Duration
+        this.durationText = this.game.add.bitmapText(90, 500, 'fontSquareBB', 'PLAY THIRTY THREE SECONDS', 22);
+        this.game.add.tween(this.durationText).to({y: 200}, 1000).easing(Phaser.Easing.Bounce.Out).delay(1750).start();
 
         // Button
         this.button = this.game.add.button(this.game.world.centerX, 525, 'startBtn', this.actionOnClickStartButton, this, 0, 0, 0);
         this.button.anchor.setTo(0.5, 0.5);
-        this.game.add.tween(this.button).to({y: this.game.world.centerY}, 1000).easing(Phaser.Easing.Bounce.Out).delay(1750).start();
+        this.game.add.tween(this.button).to({y: this.game.world.centerY}, 1000).easing(Phaser.Easing.Bounce.Out).delay(2000).start();
 
         /* Add sound
          ********************/
